0a1eb5e3b9e1fa99792ea449974339fffe29537e
AdithyanA <deepaadithyan56@gmail.com>
2020-10-14 15:45:41 +0530

    working mode is OK

---
+++ main.py
@@ -0,0 +1,11 @@
+# IMPORTS
+import time  # We are importing the time module to get the time
+from plyer import notification
+if __name__ == "__main__":
+	while True:
+		notification.notify(
+			title = "Please drink Water",
+			message = "fasdasj;fjasdfjaskdfhalsjkdfhalsjdfhalsdfjhasdhf",
+			timeout = 10
+		)
+		# time.sleep(60*60)
